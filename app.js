0ae23f729c62ec746ecc31eb94eb548d46560273
PanatChuangtuk <panat32@hotmail.com>
2024-06-11 17:20:02 +0700

    updatate

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 productsRouter.route('/').get((req, res) => {
-  res.render('products', products);
+  res.render('products', { products });
 });
 
 productsRouter.route('/:id').get((req, res) => {
