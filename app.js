9c1b323839db24fec937f1d8e8e789ffdae26cea
PanatChuangtuk <panat32@hotmail.com>
2024-06-11 14:47:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const chalk = require('chalk');
 const path = require('path');
+const productsRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,7 +14,15 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.get('/products');
+productsRouter.route('/').get((req, res) => {
+  res.render('Hello Products 1');
+});
+
+productsRouter.route('/1').get((req, res) => {
+  res.render('Hello Products 2');
+});
+
+app.use('/products', productsRouter);
 
 app.get('/', (req, res) => {
   res.render('index', {
